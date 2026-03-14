a10e26554b0ca8f9389e445a0d56e3c2099c207e
SolSwap Developer <dev@solswap.cloud>
2026-03-14 23:27:06 +0000

    fix: replace birdeye-proxy with GeckoTerminal OHLCV API (free, no key)

--- src/utils/config/birdeyeAPI.ts
+++ src/utils/config/birdeyeAPI.ts
@@ -1,43 +1,65 @@
-// birdeye api key
-export const birdeyHost = 'https://birdeye-proxy.solswap.cloud'
+// GeckoTerminal API timeframe mapping
+export const geckoTimeframe: Record<string, { timeframe: string; aggregate: number }> = {
+  '15m': { timeframe: 'minute', aggregate: 15 },
+  '1H': { timeframe: 'hour', aggregate: 1 },
+  '4H': { timeframe: 'hour', aggregate: 4 },
+  '1D': { timeframe: 'day', aggregate: 1 },
+  '1W': { timeframe: 'day', aggregate: 7 }
+}
 
+/**
+ * For GeckoTerminal, we need a pool address which we don't have here.
+ * We return a custom gecko:// protocol URL that the hook fetcher will intercept 
+ * to perform a pool lookup first.
+ */
 export const birdeyeKlineApiAddress = ({
   baseMint,
   quoteMint,
-  timeType,
-  timeFrom,
-  timeTo
+  timeType
 }: {
   baseMint: string
   quoteMint: string
   timeType: string
   timeFrom: number
   timeTo: number
-}) =>
-  `${birdeyHost}/defi/ohlcv/base_quote?base_address=${baseMint}&quote_address=${quoteMint}&type=${timeType}&time_from=${timeFrom}&time_to=${timeTo}`
+}) => {
+  const config = geckoTimeframe[timeType] || geckoTimeframe['15m']
+  return `gecko://solana/${baseMint}/${quoteMint}/${config.timeframe}/${config.aggregate}/1000/${timeType}`
+}
 
+/**
+ * Direct GeckoTerminal OHLCV URL using pool address.
+ */
 export const birdeyePairVolumeApiAddress = ({
   poolAddress,
-  timeType,
-  timeFrom,
-  timeTo
+  timeType
 }: {
   poolAddress: string
   timeType: string
   timeFrom: number
   timeTo: number
-}) => `${birdeyHost}/defi/ohlcv/pair?address=${poolAddress}&type=${timeType}&time_from=${timeFrom}&time_to=${timeTo}`
+}) => {
+  const config = geckoTimeframe[timeType] || geckoTimeframe['4H']
+  return `https://api.geckoterminal.com/api/v2/networks/solana/pools/${poolAddress}/ohlcv/${config.timeframe}?aggregate=${config.aggregate}&limit=1000&currency=usd&token=base`
+}
 
+/**
+ * Use GeckoTerminal for historical token prices as well.
+ * Since we don't have a pool address here, we use the same gecko:// schema.
+ */
 export const birdeyePairPriceApiAddress = ({
   baseMint,
-  timeType,
-  timeFrom,
-  timeTo
+  timeType
 }: {
   baseMint: string
   timeType: string
   timeFrom: number
   timeTo: number
-}) => `${birdeyHost}/defi/history_price?address=${baseMint}&type=${timeType}&address_type=token&time_from=${timeFrom}&time_to=${timeTo}`
+}) => {
+  const config = geckoTimeframe[timeType] || geckoTimeframe['4H']
+  // We use quote as SOL for a general price against USD 
+  return `gecko://solana/${baseMint}/So11111111111111111111111111111111111111112/${config.timeframe}/${config.aggregate}/1000/${timeType}`
+}
 
-export const birdeyePriceUrl = `${birdeyHost}/defi/multi_price`
+export const birdeyePriceUrl = `https://price.jup.ag/v4/price`
+
