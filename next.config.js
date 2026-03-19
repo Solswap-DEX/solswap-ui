742374305158c95bc60642070cb1216c083f405c
SolSwap Developer <dev@solswap.cloud>
2026-03-19 16:41:15 +0000

    fix: add @lifi/widget to transpilePackages for CSS import support

--- next.config.js
+++ next.config.js
@@ -14,6 +14,8 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
+  // Required so Next.js can handle global CSS imported by @lifi/widget
+  transpilePackages: ['@lifi/widget'],
   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
