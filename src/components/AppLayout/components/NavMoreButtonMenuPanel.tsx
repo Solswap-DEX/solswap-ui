1eeec5058c5d3bfe004c5b6146c63d099896b919
Solswap Admin <admin@solswap.cloud>
2026-04-05 19:48:27 +0000

    style: use space-evenly for social icons to be equidistant

--- src/components/AppLayout/components/NavMoreButtonMenuPanel.tsx
+++ src/components/AppLayout/components/NavMoreButtonMenuPanel.tsx
@@ -66,7 +66,7 @@ export function NavMoreButtonMenuPanel() {
           </Link>
         </MenuItem>
       </Box>
-      <Flex mb={-1} mt={1} py={2} justifyContent={'space-around'} alignItems="center" color={colors.textSecondary} bg={colors.backgroundTransparent07}>
+      <Flex mb={-1} mt={1} py={2} justifyContent={'space-evenly'} alignItems="center" color={colors.textSecondary} bg={colors.backgroundTransparent07}>
         <Link as={NextLink} href="https://x.com/SolswapDEX" isExternal>
           <TwitterMediaIcon width={36} height={36} />
         </Link>
